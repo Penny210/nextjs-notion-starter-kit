588da376da35f540e432fecd74c893d1eaf4123f
Penny210 <114754614+Penny210@users.noreply.github.com>
2022-10-01 00:09:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'bfbb8f9a9bf0486c8b1ee9259174c70e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Journal',
+  domain: 'https://www.notion.so/bfbb8f9a9bf0486c8b1ee9259174c70e?v=a62783b255a04d189ce3f0514bd93a9e',
+  author: '旖旎 张',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
